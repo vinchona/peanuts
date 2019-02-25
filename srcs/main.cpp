836246fc15f4a72fb7ca3ca0903ce399de6844a3
Arthur VINCHON <vinchon.arthur@gmail.com>
2019-02-25 22:42:10 +0100

    add safe main

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,6 +1,28 @@
 #include <cstdlib>
+#include <iostream>
+#include <stdexcept>
 
-int main()
+using namespace std;
+
+#define MAYBE_UNUSED(var) (void)(var)
+
+static void safe_main(int arg_count, char* arg_value[])
 {
-  return EXIT_SUCCESS;
+	MAYBE_UNUSED(arg_count);
+	MAYBE_UNUSED(arg_value);
+	return;
+}
+
+int main(int arg_count, char* arg_value[])
+{
+	try
+	{
+		safe_main(arg_count, arg_value);
+		return EXIT_SUCCESS;
+	}
+	catch(runtime_error const& error)
+	{
+		cerr << endl << "Exception caught: " << error.what() << endl;
+		return EXIT_FAILURE;
+	}
 }
