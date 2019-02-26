5dda776e549faf51f5e8ec6b0d536bbff04a2337
Arthur VINCHON <vinchon.arthur@gmail.com>
2019-02-26 21:48:15 +0100

    cosmetics

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -8,21 +8,21 @@ using namespace std;
 
 static void safe_main(int arg_count, char* arg_value[])
 {
-	MAYBE_UNUSED(arg_count);
-	MAYBE_UNUSED(arg_value);
-	return;
+  MAYBE_UNUSED(arg_count);
+  MAYBE_UNUSED(arg_value);
+  return;
 }
 
 int main(int arg_count, char* arg_value[])
 {
-	try
-	{
-		safe_main(arg_count, arg_value);
-		return EXIT_SUCCESS;
-	}
-	catch(runtime_error const& error)
-	{
-		cerr << endl << "Exception caught: " << error.what() << endl;
-		return EXIT_FAILURE;
-	}
+  try
+  {
+    safe_main(arg_count, arg_value);
+    return EXIT_SUCCESS;
+  }
+  catch (runtime_error const& error)
+  {
+    cerr << endl << "Exception caught: " << error.what() << endl;
+    return EXIT_FAILURE;
+  }
 }
