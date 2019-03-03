a9fad86220df1f175f912b637b38fbd37fef970b
Arthur VINCHON <vinchon.arthur@gmail.com>
2019-03-03 22:08:29 +0100

    add 1 more unittest

--- srcs/example.cpp
+++ srcs/example.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
 #include <peanuts.hpp>
 
-PEANUTS_UNITTEST("This is a description") {}
+PEANUTS_UNITTEST("Test 1 - This is a description") {}
+PEANUTS_UNITTEST("Test 2 - This is a description") {}
