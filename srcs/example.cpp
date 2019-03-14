157edf7c09618395c2f43c55b4809e94f8682749
Arthur VINCHON <vinchon.arthur@gmail.com>
2019-03-14 23:43:40 +0100

    extract random generation

--- srcs/example.cpp
+++ srcs/example.cpp
@@ -1,7 +1,8 @@
 #include <peanuts.hpp>
 
-#include <cassert>
+#include <cstdio>
 #include <iostream>
+#include <string>
 
 PEANUTS_TEST("Adding test description") { std::cout << "Executing unittest" << std::endl; }
 
@@ -12,6 +13,9 @@ PEANUTS_FUZZ("Adding fuzz decription")
   static int count = 0;
   std::cout << "Executing fuzz test[" << count << "]" << std::endl;
   std::cout << "size:" << peanuts_fuzz_size << std::endl;
-  std::cout << "data:" << peanuts_fuzz_data << std::endl;
+  std::cout << "data:" << peanuts_fuzz_size << std::endl;
+  for (size_t size = 0; size < peanuts_fuzz_size; size++)
+    printf("%c", peanuts_fuzz_data[size]);
+  std::cout << std::endl;
   count++;
 }
--- srcs/peanuts.cpp
+++ srcs/peanuts.cpp
@@ -35,31 +35,44 @@ int peanuts::Fuzzer::add(std::function<void(size_t, char const*)> function, char
 
 int peanuts::Fuzzer::count() { return tests.size(); }
 
+static std::string generate_random(size_t seed, size_t size_max)
+{
+  std::mt19937 size_generator{seed};
+  std::uniform_int_distribution<size_t> size_distribution{0, size_max};
+  size_t length = size_distribution(size_generator);
+  std::cout << length << std::endl;
+  std::string data{};
+  std::mt19937 random_value{length};
+  std::uniform_int_distribution<char> distribution{CHAR_MIN, CHAR_MAX};
+  for (size_t value = 0; value < length; value++)
+    data += std::string{distribution(random_value)};
+  return data;
+}
+
+static std::string generate_empty_string(size_t, size_t) { return std::string{}; }
+
 void peanuts::Fuzzer::execute(Combinatorial combinatorial)
 {
-  (void)combinatorial;
-  for (auto const& test : tests)
-  {
-    std::cout << test.description << std::endl;
-    for (size_t seed = 0; seed < 1000; seed++)
-    {
-      std::mt19937 size_generator{seed};
-      std::uniform_int_distribution<> size_distribution{INT_MIN, INT_MAX};
-      char length = size_distribution(size_generator);
-      std::string data{};
-      std::mt19937 random_value{length};
-      std::uniform_int_distribution<char> distribution{CHAR_MIN, CHAR_MAX};
-      for (char value = 0; value < length; value++)
-        data += std::string{distribution(random_value)};
+  std::function<std::string(size_t, size_t)> generate_data = generate_empty_string;
 
-      try
+  if (combinatorial == Combinatorial::random)
+    generate_data = generate_random;
+
+  size_t size_max = UCHAR_MAX;
+
+  for (unsigned seed = 0;; seed++)
+  {
+    auto data = generate_data(seed, size_max);
+    try
+    {
+      for (auto const& test : tests)
       {
         test.function(data.length(), data.c_str());
       }
-      catch (std::exception const& exception)
-      {
-        std::cerr << exception.what() << std::endl;
-      }
+    }
+    catch (std::exception const& exception)
+    {
+      std::cerr << exception.what() << std::endl;
     }
   }
 }
