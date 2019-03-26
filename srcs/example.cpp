22145739b598c83641c9767692cbe5db4d5c59dc
Arthur Vinchon <arthur.vinchon@allegrodvt.com>
2019-03-26 17:34:16 +0100

    enhance example

--- srcs/example.cpp
+++ srcs/example.cpp
@@ -4,17 +4,26 @@
 #include <iostream>
 #include <string>
 
-PEANUTS_TEST("Adding test description") { std::cout << "Executing unittest" << std::endl; }
+PEANUTS_TEST("Adding test description") { std::cout << "Executing test" << std::endl; }
 
-PEANUTS_TEST("Adding other test description") { std::cout << "Executing other unittest" << std::endl; }
+PEANUTS_TEST("Adding another test description") { std::cout << "Executing another test" << std::endl; }
 
 PEANUTS_FUZZ("Adding fuzz decription")
 {
-  static int count = 0;
+  std::cout << "Executing fuzztest" << std::endl;
   std::cout << "size:" << peanuts_fuzz_size << std::endl;
   std::cout << "data:" << peanuts_fuzz_size << std::endl;
   for (size_t size = 0; size < peanuts_fuzz_size; size++)
     printf("%c", peanuts_fuzz_data[size]);
-  std::cout << std::endl;
-  count++;
+  std::cout << std::endl << std::flush;
+}
+
+PEANUTS_FUZZ("Adding another fuzz decription")
+{
+  std::cout << "Executing another fuzztest" << std::endl;
+  std::cout << "size:" << peanuts_fuzz_size << std::endl;
+  std::cout << "data:" << peanuts_fuzz_size << std::endl;
+  for (size_t size = 0; size < peanuts_fuzz_size; size++)
+    printf("%c", peanuts_fuzz_data[size]);
+  std::cout << std::endl << std::flush;
 }
