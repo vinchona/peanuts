fc13c2c40cda4ad28695a66181c408fc6f2f146b
Arthur Vinchon <arthur.vinchon@allegrodvt.com>
2019-04-19 16:49:38 +0200

    implement is_equals and is_not_equals

--- include/validator.hpp
+++ include/validator.hpp
@@ -8,6 +8,19 @@ namespace peanuts
 
 template<typename T, typename U> struct Validator
 {
+  bool is_equals(T const expected, U actual)
+  {
+    if(expected == (T)actual)
+      return true;
+    return false;
+  }
+
+  bool is_not_equals(T const expected, U actual)
+  {
+    if(expected != (T)actual)
+      return true;
+    return false;
+  }
 };
 
 } // namespace peanuts
