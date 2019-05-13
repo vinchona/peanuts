7e348d70b76cb692cc0d224696d6cbbda4a15c7f
Arthur VINCHON <vinchon.arthur@gmail.com>
2019-05-13 21:22:27 +0200

    reformate

--- includes/peanuts.hpp
+++ includes/peanuts.hpp
@@ -3,10 +3,10 @@
 #ifndef PEANUTS_HPP
 #define PEANUTS_HPP
 
-#include <registrant.hpp>
-#include <validator.hpp>
 #include <cstdlib>
 #include <iostream>
+#include <registrant.hpp>
+#include <validator.hpp>
 
 #define PEANUTS_CONCATENATES2(a, b) a##b
 #define PEANUTS_CONCATENATES(a, b) PEANUTS_CONCATENATES2(a, b)
@@ -34,32 +34,32 @@
 #define PEANUTS_TEST(description) PEANUTS_DECLARE_AND_MAKE_UNIQUE_UNITTEST(description, __LINE__)
 #define PEANUTS_FUZZ(description) PEANUTS_DECLARE_AND_MAKE_UNIQUE_FUZZTEST(description, __LINE__)
 
-#define PEANUTS_IS_EQUAL_OR_ABORT_WITH_DEBUG(expected, actual, file, line, actual_name) \
-  do \
-{ \
-  if(!peanuts::Validator::is_equal(expected, actual)) \
-  {\
-    std::cerr << "[" file << ":" << line << "] error: "<< actual_name << " '" << actual << "' is not equals to '"<< expected << "'" << std::endl; \
-    ::abort(); \
-  }\
-} while(0)
+#define PEANUTS_IS_EQUAL_OR_ABORT_WITH_DEBUG(expected, actual, file, line, actual_name)                                \
+  do                                                                                                                   \
+  {                                                                                                                    \
+    if (!peanuts::Validator::is_equal(expected, actual))                                                               \
+    {                                                                                                                  \
+      std::cerr << "[" file << ":" << line << "] error: " << actual_name << " '" << actual << "' is not equals to '"   \
+                << expected << "'" << std::endl;                                                                       \
+      ::abort();                                                                                                       \
+    }                                                                                                                  \
+  } while (0)
 
-#define PEANUTS_IS_EQUAL_OR_ABORT(expected, actual) \
+#define PEANUTS_IS_EQUAL_OR_ABORT(expected, actual)                                                                    \
   PEANUTS_IS_EQUAL_OR_ABORT_WITH_DEBUG(expected, actual, __FILE__, __LINE__, #actual)
 
-#define PEANUTS_IS_NOT_EQUAL_OR_ABORT_WITH_DEBUG(expected, actual, file, line, actual_name) \
-  do \
-{ \
-  if(!peanuts::Validator::is_not_equal(expected, actual)) \
-  {\
-    std::cerr << "[" file << ":" << line << "] error: " << actual_name << " '" << actual << "' is equals to '"<< expected << "'" << std::endl; \
-    ::abort(); \
-  }\
-} while(0)
+#define PEANUTS_IS_NOT_EQUAL_OR_ABORT_WITH_DEBUG(expected, actual, file, line, actual_name)                            \
+  do                                                                                                                   \
+  {                                                                                                                    \
+    if (!peanuts::Validator::is_not_equal(expected, actual))                                                           \
+    {                                                                                                                  \
+      std::cerr << "[" file << ":" << line << "] error: " << actual_name << " '" << actual << "' is equals to '"       \
+                << expected << "'" << std::endl;                                                                       \
+      ::abort();                                                                                                       \
+    }                                                                                                                  \
+  } while (0)
 
-#define PEANUTS_IS_NOT_EQUAL_OR_ABORT(expected, actual) \
+#define PEANUTS_IS_NOT_EQUAL_OR_ABORT(expected, actual)                                                                \
   PEANUTS_IS_NOT_EQUAL_OR_ABORT_WITH_DEBUG(expected, actual, __FILE__, __LINE__, #actual)
-
-
 
 #endif /* __PEANUTS__ */
--- includes/validator.hpp
+++ includes/validator.hpp
@@ -7,19 +7,12 @@ namespace peanuts
 {
 struct Validator
 {
-  template <typename T, typename U>
-    static
-    bool is_equal(T&& expected, U&& actual)
-    {
-      return (expected == (T)actual);
-    }
+  template <typename T, typename U> static bool is_equal(T&& expected, U&& actual) { return (expected == (T)actual); }
 
-  template <typename T, typename U>
-    static
-    bool is_not_equal(T&& expected, U&& actual)
-    {
-      return (expected != (T)actual);
-    }
+  template <typename T, typename U> static bool is_not_equal(T&& expected, U&& actual)
+  {
+    return (expected != (T)actual);
+  }
 };
 
 } // namespace peanuts
